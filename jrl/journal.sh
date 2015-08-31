3832660bb9c81db23a5590fe702ec97ab1bf7a14
Charlie Drage <charlie@charliedrage.com>
2015-08-31 11:03:40 -0400

    update

--- jrl/journal.sh
+++ jrl/journal.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+cd $HOME
 echo -n "[enc] password for file: "
 read -s var
-openssl aes-256-cbc -d -salt -in txt.aes -out /tmp/txt.txt -k $var
+openssl aes-256-cbc -d -salt -in /root/txt.aes -out /tmp/txt.txt -k $var
 
 if [ $? -eq 0 ]; then
 	echo OK
 	vim +$ +startinsert /tmp/txt.txt
-	openssl aes-256-cbc -salt -in /tmp/txt.txt -out txt.aes -k $var
+	openssl aes-256-cbc -salt -in /tmp/txt.txt -out /root/txt.aes -k $var
 else
 	echo FAIL
 fi
