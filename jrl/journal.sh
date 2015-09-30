a7c93f3b589b227dfe0fa4ac27c788d8ca165f24
Charlie Drage <charlie@charliedrage.com>
2015-09-30 04:33:01 -0400

    update

--- jrl/journal.sh
+++ jrl/journal.sh
@@ -6,7 +6,7 @@ openssl aes-256-cbc -d -salt -in /root/txt.aes -out /tmp/txt.txt -k $var
 
 if [ $? -eq 0 ]; then
 	echo OK
-  time=`date +'%G-%m-%d %H:%M'`"\n"
+  time="\n"`date +'%G-%m-%d %H:%M'`"\n"
   echo -e $time >> /tmp/txt.txt
 	vim +$ +startinsert /tmp/txt.txt
 	openssl aes-256-cbc -salt -in /tmp/txt.txt -out /root/txt.aes -k $var
