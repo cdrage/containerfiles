64f6317ed0e254ccddc2a82a0eee82e3e0bb511f
Charlie Drage <charlie@charliedrage.com>
2016-02-26 18:11:01 -0500

    Update jrl dockerfile

--- jrl/journal.sh
+++ jrl/journal.sh
@@ -2,14 +2,14 @@
 cd $HOME
 echo -n "[enc] password for file: "
 read -s var
-openssl aes-256-cbc -d -salt -in /root/txt.aes -out /tmp/txt.txt -k $var
+openssl aes-256-cbc -d -salt -in /root/txt.enc -out /tmp/txt.txt -k $var
 
 if [ $? -eq 0 ]; then
 	echo OK
   time="\n"`date +'%G-%m-%d %H:%M'`"\n"
   echo -e $time >> /tmp/txt.txt
 	vim +$ +startinsert /tmp/txt.txt
-	openssl aes-256-cbc -salt -in /tmp/txt.txt -out /root/txt.aes -k $var
+	openssl aes-256-cbc -salt -in /tmp/txt.txt -out /root/txt.enc -k $var
 else
 	echo FAIL
 fi
