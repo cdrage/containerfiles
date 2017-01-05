6eba31ccdd228e45704f88a215ce60518cdcf1b6
Charlie Drage <charlie@charliedrage.com>
2017-01-05 15:09:32 -0500

    add matterhorn

---
+++ matterhorn/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Replace with the appropriate environment variables
+sed -i "s,%MM_USER%,$MM_USER,g" "/config.ini" 
+sed -i "s,%MM_HOST%,$MM_HOST,g" "/config.ini" 
+sed -i "s,%MM_PORT%,$MM_PORT,g" "/config.ini" 
+
+exec "$@"
