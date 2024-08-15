a29627d99dea0f3d17b1f93f6812cc85b846ee6d
Charlie Drage <charlie@charliedrage.com>
2024-08-15 14:59:06 -0400

    update
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- vulkan-mac-silicon-gpu-vkcube/startup.sh
+++ vulkan-mac-silicon-gpu-vkcube/startup.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# Start Xvfb
-#Xvfb :0 -screen 0 1024x768x24 -nolisten tcp &
-
-#sleep 2
-
 # Set DISPLAY environment variable
 export DISPLAY=:1
 
 # Start VNC server with no password
 vncserver :1 -geometry 1024x768 -localhost -SecurityTypes None &
-sleep 1
-#/usr/share/novnc/utils/novnc_proxy --vnc localhost:5901 &> /var/log/novnc.log &
+
+# Sleep to wait for initialization
+sleep 2
+
+# Start noVNC server
 /usr/local/bin/novnc_proxy --vnc localhost:5901 --listen 6080 &
 
-nginx
+# Sleep to wait for initialization
+sleep 2
 
+# In the future switch to using virtio-gpu which may eventually support gpu modules
 export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/lvp_icd.aarch64.json
-
-sleep 5
 vkcube
